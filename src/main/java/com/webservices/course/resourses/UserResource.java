621fba755efa674bfdfb50ce1db90ab2d55867a4
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-01-30 23:44:43 -0300

    findAll e findById como serviços

--- src/main/java/com/webservices/course/resourses/UserResource.java
+++ src/main/java/com/webservices/course/resourses/UserResource.java
@@ -1,10 +1,15 @@
 package com.webservices.course.resourses;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.webservices.course.entities.User;
+import com.webservices.course.services.UserServices;
 
 
     //!Camada de recursos (resource layer) - controladores Rest!
@@ -13,11 +18,24 @@ import com.webservices.course.entities.User;
 @RestController
 @RequestMapping(value = "/users")       
 public class UserResource {
+
+    @Autowired
+    private UserServices service; // Injeção de dependência para o serviço*
     
     @GetMapping
-    public ResponseEntity <User> findAll(){  //*O método findAll é um endpoint para acessar os usuários*//
-        User u = new User (1L, "Maria", "maria@gmail.com", "99999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity <List<User>> findAll(){  //O método findAll é um endpoint para acessar os usuários*//
+        
+        List<User> list = service.findAll(); // O método findAll é um endpoint para acessar os usuários*
+        
+        return ResponseEntity.ok().body(list); // Retorna a resposta com sucesso*
+    }
+
+    @GetMapping(value = "/{id}") // O método findById é um endpoint para acessar um usuário por Id*
+    public ResponseEntity <User> findById(@PathVariable Long id){
+        
+        User obj = service.findById(id);
+        
+        return ResponseEntity.ok().body(obj); // Retorna a resposta com sucesso*
     }
 }
 
---
+++ src/main/java/com/webservices/course/services/UserServices.java
@@ -0,0 +1,24 @@
+package com.webservices.course.services;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.webservices.course.entities.User;
+import com.webservices.course.repositories.UserRepository;
+
+@Service
+public class UserServices { // Classe de serviço
+    
+    @Autowired
+    private UserRepository repository; // Injeção de dependência
+
+    public List<User> findAll(){ // Método para retornar todos os usuários
+        return repository.findAll(); // Retorna todos os usuários
+    }
+
+    public User findById(Long Id){ // Método para retornar um usuário por Id
+        Optional<User> obj = repository.findById(Id); // Retorna um usuário por Id
+        return obj.get(); // Retorna o usuário
+    }
+}
