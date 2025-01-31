ff8144547588def8c17d0bf8e312d6bd782f280a
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-01-30 23:03:42 -0300

    Repositório JPA, injeção de dependência, TestConfig

---
+++ src/main/java/com/webservices/course/config/TestConfig.java
@@ -0,0 +1,29 @@
+package com.webservices.course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import com.webservices.course.entities.User;
+import com.webservices.course.repositories.UserRepository;
+
+@Configuration //especificar que é uma classe de configuração para o spring
+@Profile("test")
+public class TestConfig implements CommandLineRunner{
+    
+    @Autowired   //injeção de dependência
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User u1 = new User(null, "Maria", "maria@gmail.com", "97777777777", "12344568");
+        User u2 = new User(null, "Carlos", "carlos@gmail.com", "99999999999", "12344568");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+
+
+}
